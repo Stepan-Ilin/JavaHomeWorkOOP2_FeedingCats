c921d58ce6a65f52fe17aa8e53278cd5202cdd1b
Stepan Ilin <stepa_ilin@mail.ru>
2024-03-10 12:56:25 +0300

    Добавленно reductionFood(int howMuch) в Plate.java Измененно eat(Plate plate) в Cat.java маханизма кормления для более реального представления о сущностях Сат и Plate.

--- src/Plate.java
+++ src/Plate.java
@@ -1,24 +1,27 @@
 public class Plate {
     private int food;
+
     public Plate(int food) {
         this.food = food;
     }
+
     public void info() {
         System.out.println("plate: " + food);
     }
 
     //region Метод добавления еды в тарелку
-    public void addFood(int food){
-        if(food>0)
+    public void addFood(int food) {
+        if (food > 0)
             this.food += food;
     }
-    //endregion
 
-    public int getFood() {
-        return food;
-    }
-
-    public void setFood(int food) {
-        this.food = food;
+    /**
+     * Убавление еды из тарелки
+     * @param howMuch кол-во еды, на которое должно быть насыщение (в условных единицах)
+     * @return кол-во еды в тарелке, после насыщении. В отрицательном значении - кол-во
+     * еды, которое не хватило для насыщения, при этом кол-во еды в тарелке НЕ МЕНЯЕТСЯ
+     */
+    public int reductionFood(int howMuch) {
+        return this.food < howMuch ? this.food - howMuch : (this.food -= howMuch);
     }
 }
